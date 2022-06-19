75e61c1687df099dc9159273ef6e49d5b607741a
Matt Bowen <mattbowen03@gmail.com>
2022-06-19 14:58:10 -0500

    added dropdown patient nav, updated copy

--- scripts/app.js
+++ scripts/app.js
@@ -28,6 +28,26 @@ menuItems.forEach(
   }
 )
 
+//hides close dropdown button on initial page load
 const startClosed = () => closeIcon.style.display = "none";
-
 startClosed();
+
+//dropdown for patient nav link
+function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("sdd");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('sdd')) {
+        openDropdown.classList.remove('sdd');
+      }
+    }
+  }
+}
+
