1f8abf0530012444a640ba8a6ae72199b1ab25e1
Matt Bowen <mattbowen03@gmail.com>
2022-07-24 09:19:46 -0500

    update our services toogle view and card buttons

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,15 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const toggleView = document.querySelector(".toggle-view");
+const hiddenItems = document.querySelectorAll(".hidden-items");
 
-function toggleMenu() {
-  console.log('Hamburger clicked, toggleMenu function run');
-
-  if (menu.classList.contains("showMenu")) {
-    console.log('menu.classList.contains showMenu');
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
-}
-
-hamburger.addEventListener("click", toggleMenu);
+//toggle see more in our services
+//bc queryselectorAll returns an array of elements, you have to use an iterative fn to apply the class changes to each item of the array
+toggleView.addEventListener('click', () => {
+  hiddenItems.forEach(item => {
+    item.classList.toggle('hidden');
+  })
+  hiddenItems[0].classList.contains('hidden') ? toggleView.innerHTML = 'See full list...' : toggleView.innerHTML = 'Hide full list...';
+})
 
 
-menuItems.forEach( 
-  function(menuItem) { 
-    menuItem.addEventListener("click", toggleMenu);
-  }
-)
 
-//hides close dropdown button on initial page load
-const startClosed = () => closeIcon.style.display = "none";
-startClosed();
-
-//dropdown for patient nav link
-function dropDown() {
-  document.getElementById("myDropdown").classList.toggle("sdd");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('sdd')) {
-        openDropdown.classList.remove('sdd');
-      }
-    }
-  }
-}
 
