c2e284d52023cd66836d13e9ac274954c5208ee9
Matt Bowen <mattbowen03@gmail.com>
2022-07-25 22:34:50 -0500

    added a hero slider

--- scripts/app.js
+++ scripts/app.js
@@ -14,4 +14,49 @@ toggleView.addEventListener('click', () => {
 btn.forEach(button => button.addEventListener('click', () => window.location.href = './appointment-form.html'))
 
 
+//Hero Slider----------
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+showSlides1();
+
+function showSlides1() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides1, 12000); // Change image every 12 seconds
+}
+
+
 
